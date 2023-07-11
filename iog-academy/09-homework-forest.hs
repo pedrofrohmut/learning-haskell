c9aa01b0f766710ec843fd68706ebd0d0b233734
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-07-11 14:32:28 -0300

    IOG - 09 homework forest - initial

--- iog-academy/09-homework-forest.hs
+++ iog-academy/09-homework-forest.hs
@@ -44,3 +44,48 @@ Using GHCi, like the "Maze" game, this game should look like this:
 *Main> solveForest testForest [GoForward, GoLeft , GoRight]
 "YOU'VE FOUND THE EXIT!!"
 -}
+
+
+data Move = GoFoward | GoLeft | GoRight deriving (Eq)
+
+type Forest = [Move]
+type PlayerMoves = [Move]
+
+{-
+    TODO:
+    - You start the forest on Point2 (0, 0)
+    - The exit is a secret coordinate where y >=0 and x can be any value
+    - To be possible to play we use only integer numbers (floating will be almost impossible)
+    - The first version of this game will have the same stamina cost for every terrain
+    - Maybe add a hint like to tell the player when he is near the exit
+    - Make a forest generator
+    - Make a debug option so it will show the answer in debug mode
+-}
+solveForest :: Forest -> PlayerMoves -> String
+solveForest forestMap playerMoves
+    | null forestMap = "The forest is empty and this adventure is pointless"
+    | null playerMoves = "You are now inside the Forest. \nYou have 10 stamina, choose your next move."
+    | otherwise = "TODO"
+    -- | otherwise = solve forest playerMoves
+    -- where
+    --     solve forest moves
+        -- TODO: make it a recursive function that will match forest and moves head
+
+introStr :: String
+introStr =
+    "Hello, brave adventurer. You are now inside the Forest!\n\
+    \Now you need to provide the forest you choose to claim your victory upon \
+    \and the path you will follow.\n\
+    \The adventurer path is an array of moves, and you have 3 choices: \
+    \GoRight, GoLeft and GoForward.\n\
+    \To make I try you have 10 stamina points the you spend to cross the Forest \
+    \terrains.\n\
+    \Stamina is spent more or less depending on how hard certain terrain is to transpass."
+
+-- Show the secrets and the answer so it is easier to test
+debugMode :: Bool
+debugMode = True
+
+main :: IO ()
+main = do
+    putStrLn $ (if debugMode then "# DEBUG MODE #\n" else "") ++ introStr
