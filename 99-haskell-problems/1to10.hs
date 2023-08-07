9d3864924782f83930db67a83b9a3e064e5dc7d2
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-08-07 14:04:18 -0300

    99 - Prob 03

--- 99-haskell-problems/1to10.hs
+++ 99-haskell-problems/1to10.hs
@@ -1,10 +1,17 @@
--- Problem 01
+-- 01. Last elem of a list
 myLast :: [a] -> a
 myLast (x:xs)
     | null xs = x
     | otherwise = myLast xs
 
+-- 02. Second last elem of a list
 myButLast :: [a] -> a
 myButLast (x1:x2:xs)
     | null xs = x1
     | otherwise = myButLast (x2:xs)
+
+-- 03. Nth elem of a list
+elemAt :: [a] -> Int -> a
+elemAt (x:xs) n
+    | n == 0 = x
+    | otherwise = elemAt xs (n - 1)
