5dcfb369f8735bdd2e8e9bddb8161f4dc7fe2541
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-08-07 13:58:11 -0300

    99 - Prob 2

--- 99-haskell-problems/1to10.hs
+++ 99-haskell-problems/1to10.hs
@@ -3,3 +3,8 @@ myLast :: [a] -> a
 myLast (x:xs)
     | null xs = x
     | otherwise = myLast xs
+
+myButLast :: [a] -> a
+myButLast (x1:x2:xs)
+    | null xs = x1
+    | otherwise = myButLast (x2:xs)
