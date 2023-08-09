85d76f7a2acc8962595ab20c15aafccdac537eaf
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-08-09 08:57:53 -0300

    99 - Prob 07

--- 99-haskell-problems/1to10.hs
+++ 99-haskell-problems/1to10.hs
@@ -35,6 +35,18 @@ isPolindrome :: Eq a => [a] -> Bool
 isPolindrome list = list == (reverse list)
 
 -- 07 Flatten a nested list structure
+-- λ> flatten (Elem 5)
+-- [5]
+-- λ> flatten (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]])
+-- [1,2,3,4,5]
+-- λ> flatten (List [])
+-- []
+data NestedList a = Elem a | List [NestedList a] deriving Show
+
+flatten :: NestedList a -> [a]
+flatten (Elem x) = [x]
+flatten (List []) = []
+flatten (List (x:xs)) = flatten x ++ flatten (List xs)
 
 -- 08 Eliminate consecutive duplicates of list elements
 
