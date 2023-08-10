675cdcec6907eee4ae0cc00cd6ef5d03696477a0
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-08-10 10:35:52 -0300

    99 - Prob 09

--- 99-haskell-problems/1to10.hs
+++ 99-haskell-problems/1to10.hs
@@ -19,16 +19,20 @@ elemAt (x:xs) n
 -- 04 Find the number of elements in a list
 myLength :: [a] -> Int
 myLength list =
-    let count [] n = n
+    let
+        count [] n = n
         count (y:ys) n = count ys (n + 1)
-    in  count list 0
+    in
+        count list 0
 
 -- 05 Reverse a list
 myReverse :: [a] -> [a]
 myReverse list =
-    let reverseIt result [] = result
+    let
+        reverseIt result [] = result
         reverseIt result (x:xs) = reverseIt (x : result) xs
-    in  reverseIt [] list
+    in
+        reverseIt [] list
 
 -- 06 Find out whether a list is a palindrome
 isPolindrome :: Eq a => [a] -> Bool
@@ -53,12 +57,31 @@ myFlatten (List (x:xs)) = myFlatten x ++ myFlatten (List xs)
 -- "1234567"
 myComp :: Eq a => [a] -> [a]
 myComp (x:xs) =
-    let comp res curr (y:ys)
+    let
+        comp res curr (y:ys)
             | null ys = if y == curr then res else res ++ [y]
             | y == curr = comp res curr ys
             | otherwise = comp (res ++ [y]) y ys
-    in  comp [x] x xs
+    in
+        comp [x] x xs
 
 -- 09 Pack consecutive duplicates of list elements into sublists
+-- * (pack '(a a a a b c c a a d e e e e))
+-- ((A A A A) (B) (C C) (A A) (D) (E E E E))
+
+t09_1 = [1,1,1,1,2,3,3,1,1,4,5,5,5]
+t09_2 = ['a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e']
+
+myPack :: Eq a => [a] -> [[a]]
+myPack (x:xs) =
+    let
+        pack :: Eq a => [a] -> a -> [a] -> [[a]]
+        pack tmp curr (y:ys)
+            | null ys && y == curr = [y:tmp]
+            | null ys && y /= curr = [tmp, [y]]
+            | y == curr            = pack (y:tmp) curr ys
+            | y /= curr            = tmp : pack [y] y ys
+    in
+        pack [x] x xs
 
 -- 10 Run-length encoding of a list
